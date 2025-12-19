28b7aa77633fc562bb3b21aeef65815e4e35510e
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2025-12-19 21:35:48 +0200

    feat: configure CustomUserAdmin with additional profile fields

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = [
+        "username",
+        "email",
+        "discord_tag",
+        "reputation",
+        "is_staff",
+        "date_joined"
+    ]
+
+    list_filter = [
+        "is_staff",
+        "is_active",
+        "date_joined"
+    ]
+
+    search_fields = ["username", "email", "discord_tag"]
+
+    fieldsets = (
+        ("Authentication", {
+            "fields": ("username", "password")
+        }),
+        ("Personal Info", {
+            "fields": ("email", "bio", "avatar")
+        }),
+        ("Gaming Platforms", {
+            "fields": ("discord_tag", "steam_url"),
+        }),
+        ("Extra Data", {
+            "fields": ("reputation",),
+        }),
+        ("Permissions", {
+            "fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions"),
+            "classes": ("collapse",),
+        }),
+        ("Important dates", {
+            "fields": ("last_login", "date_joined"),
+            "classes": ("collapse",),
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2"),
+        }),
+    )

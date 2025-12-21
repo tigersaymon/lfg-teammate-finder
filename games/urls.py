ae828adad0eaa56590cba9fabf89d52b6a8a2dc8
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2025-12-21 23:38:02 +0200

    feat: implement games role-option view

--- games/urls.py
+++ games/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from games.views import GameListView
+from games.views import (
+    GameListView, 
+    GetGameRolesView,
+)
 
 app_name = "games"
 
 urlpatterns = [
     path("", GameListView.as_view(), name="index"),
+    path("htmx/get-game-roles/", GetGameRolesView.as_view(), name="get-game-roles"),
+
 ]
--- games/views.py
+++ games/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views import generic, View
 
-from games.models import Game
+from games.forms import UserGameProfileForm
+from games.models import Game, GameRole, UserGameProfile
 
 
 class GameListView(generic.ListView):
     model = Game
     template_name = "games/index.html"
     context_object_name = "games"
+
+
+class GetGameRolesView(View):
+    def get(self, request, *args, **kwargs):
+        game_id = request.GET.get("game")
+        roles = GameRole.objects.none()
+
+        if game_id:
+            roles = GameRole.objects.filter(game_id=game_id).order_by("order")
+
+        return render(request, "games/partials/role_options.html", {"roles": roles})
